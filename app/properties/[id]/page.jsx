5517f89a9fa8fbc5061b933cdc47f878d2595fc8
unknown <robertgicheha3@gmail.com>
2024-03-31 02:33:58 +0300

    worked on the navbar component

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,27 +1,8 @@
 'use client'
 
-import {
-  useRouter,
-  useParams,
-  useSearchParams,
-  usePathname,
-} from 'next/navigation'
-
 const page = () => {
-  const router = useRouter()
-  const searchParams = useSearchParams()
-  const { id } = useParams()
-  const name = searchParams.get('name')
-  const pathname = usePathname()
-
   console.log('Hello')
-  return (
-    <div>
-      <button onClick={() => router.push('/')} className='bg-blue-500 p-2'>
-        Go Home {pathname}
-      </button>
-    </div>
-  )
+  return <div>Homepage</div>
 }
 
 export default page
