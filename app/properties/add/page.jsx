c01c5b147eae94310ab4acfd467e89ff52dd7667
unknown <robertgicheha3@gmail.com>
2024-03-31 01:39:07 +0300

    File Based Routing and SSR

---
+++ app/properties/add/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import link from 'next/link'
+
+const PropertiesPage = () => {
+  return (
+    <div>
+      <h1 className='text-3xl'>Welcome</h1>
+      <Link href='/'>Go Home</Link>
+    </div>
+  )
+}
+
+export default PropertiesPage
